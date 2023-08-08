897d1b7d1c6486dd9d93ad6d956e3fb41a39802b
itsabubakarshf <ItsAbubakarShf@gmail.com>
2023-08-08 12:45:03 +0500

    created routes and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import NavBar from "./components/Navigation";
 import "./App.scss";
+import Shope from "./components/Shope";
+import SignIn from "./routes/signIn";
+import Contact from "./components/Contact";
 const App = () => {
   return (
     <>
       <Routes>
       <Route path="/" element={<NavBar/>} >
-        <Route path="/home" index element={<Home/>} />
+        <Route index element={<Home/>} />
+        <Route path="/shop" element={<Shope/>} />
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/contact" element={<Contact/> } />
+
         </Route>
       </Routes>
     </>
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,9 +1,30 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import "./index.scss";
+import { ReactSVG } from "react-svg";
+
 const NavBar = () => {
   return (
     <>
-      <div>NavBar</div>
+        <div className="navigation">
+          <Link className="logo-container" to="/">
+            <ReactSVG src="../../../src/assets/crown.svg"/>
+          </Link>
+          <div className="nav-links-container">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+            <Link className="nav-link" to="/shop">
+              Shope
+            </Link>
+            <Link className="nav-link" to="/contact">
+              Contact
+            </Link>
+            <Link className="nav-link" to="/sign-in">
+              SignIn
+            </Link>
+          </div>
+        </div>
       <Outlet />
     </>
   );
